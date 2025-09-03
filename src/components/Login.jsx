3b97e445b0a516c79cff5d9e20c74115c8710134
Felipe <felipeeduardofreitasm@gmail.com>
2025-09-02 21:46:23 -0300

    feature: agora os resets estao setados, preciso revisar. Tambei preciso corrigir o turnresult, para o decide winner retornar se ha vencedor do game, e com isso, alertar no frontend. :D

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,47 +1,70 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import axios from "axios";
 
 function Login({ onSwitch }) {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const login = e.target.login.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log("Enviando login", login, password, email)
+
+        try {
+            const response = await axios.post("https://localhost:5105/api/users", {
+                name: login,
+                password: password,
+                email: email,
+            });
+
+            console.log("Resposta:", response.data)
+        } catch (error) {
+            console.error("Erro na requisição", error);
+        }
+    };
+
     return (
         <>
-            <h1 className="title">Login</h1>
-            
-            
+            <form onSubmit={handleSubmit} className="authentication-container">
+                <h1 className="title">Login</h1>
+                <div className="login-container">
+                    <motion.label
+                        htmlFor="login"
+                        className="login-label"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Login:
+                    </motion.label>
+                    <input className="login-input" type="text" id="login" placeholder="Seu login" />
+                </div>
 
-            <div className="login-container">
-                <motion.label
-                    htmlFor="login"
-                    className="login-label"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Login:
-                </motion.label>
-                <input className="login-input" type="text" id="login" placeholder="Seu login" />
-            </div>
+                <div className="password-container">
+                    <motion.label
+                        htmlFor="password"
+                        className="password-label"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Senha:
+                    </motion.label>
+                    <input className="password-input" type="password" id="password" placeholder="Sua senha" />
+                </div>
 
-            <div className="password-container">
-                <motion.label
-                    htmlFor="password"
-                    className="password-label"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Senha:
-                </motion.label>
-                <input className="password-input" type="password" id="password" placeholder="Sua senha" />
-            </div>
+                <div className="remember-me-forgot-password-container">
+                    <label htmlFor="remember-me-checkbox" className="remember-me-container">
+                        <input type="checkbox" id="remember-me-checkbox" name="remember-me-checkbox" />
+                        <div className="remember-me-text">Lembrar Acesso</div>
+                    </label>
+                    <div className="forgot-password">Esqueci a senha</div>
+                </div>
 
-            <div className="remember-me-forgot-password-container">
-                <label htmlFor="remember-me-checkbox" className="remember-me-container">
-                    <input type="checkbox" id="remember-me-checkbox" name="remember-me-checkbox" />
-                    <div className="remember-me-text">Lembrar Acesso</div>
-                </label>
-                <div className="forgot-password">Esqueci a senha</div>
-            </div>
+                <button className="login-button">Entrar</button>
+            </form>
 
-            <button className="login-button">Entrar</button>
             <p>Ainda não tem registro?</p>
             <button className="register-button" style={{ width: '40%', height: '50px', fontSize: '1rem' }} onClick={onSwitch}>Registrar</button>
         </>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,47 +1,59 @@
 import { motion } from "framer-motion"
 
 function Register({ onSwitch }) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const login = e.target.login.value;
+        const password = e.target.password.value;
+        const email = e.target.email.value;
+
+        console.log("Enviando login", login, password, email)
+    };
+
     return (
         <>
-            <h1 className="title">Registro</h1>
-            <div className="login-container">
-                <motion.label
-                    htmlFor="login"
-                    className="login-label"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Login:
-                </motion.label>
-                <input className="login-input" type="text" id="login" placeholder="Seu login" />
-            </div>
+            <form onSubmit={handleSubmit} className="authentication-container">
+                <h1 className="title">Registro</h1>
+                <div className="login-container">
+                    <motion.label
+                        htmlFor="login"
+                        className="login-label"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Login:
+                    </motion.label>
+                    <input className="login-input" type="text" id="login" placeholder="Seu login" />
+                </div>
 
-            <div className="email-container">
-                <motion.label
-                    htmlFor="email"
-                    className="email-label"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Email:
-                </motion.label>
-                <input className="email-input" type="email" id="email" placeholder="Seu email" />
-            </div>
-            <div className="password-container">
-                <motion.label
-                    htmlFor="password"
-                    className="password-label"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    Senha:
-                </motion.label>
-                <input className="password-input" type="password" id="password" placeholder="Sua senha" />
-            </div>
-            <button className="register-button">Registrar</button>
+                <div className="email-container">
+                    <motion.label
+                        htmlFor="email"
+                        className="email-label"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Email:
+                    </motion.label>
+                    <input className="email-input" type="text" id="email" placeholder="Seu email" />
+                </div>
+                <div className="password-container">
+                    <motion.label
+                        htmlFor="password"
+                        className="password-label"
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Senha:
+                    </motion.label>
+                    <input className="password-input" type="password" id="password" placeholder="Sua senha" />
+                </div>
+                <button className="register-button">Registrar</button>
+            </form>
             <p>Já tem conta?</p>
             <button className="login-button" style={{ width: '40%', height: '50px', fontSize: '1rem' }} onClick={onSwitch}>Entrar</button>
         </>
